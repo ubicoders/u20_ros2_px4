ad8f36c352b0b41bc5f8b1b6ca40b7cf43376cfd
el <test@ubicoders.com>
2024-02-06 20:42:21 -0800

    aa

--- download_px4_autopilot.bash
+++ download_px4_autopilot.bash
@@ -3,7 +3,7 @@ pip3 install jsonschema
 cd /home/ubuntu
 git clone https://github.com/PX4/PX4-Autopilot --recursive
 cd PX4-Autopilot
-git checkout v1.14.0 # tag at release/1.14.0 branch
+git checkout -f v1.14.0 # tag at release/1.14.0 branch
 git submodule update --recursive
 git submodule update --init --recursive
 
